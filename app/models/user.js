57c6ef758c68631ea57517a1bb33c0f5c6394cef
Hack Reactor Students <accounts@hackreactor.com>
2014-08-23 17:08:53 -0700

    Reconfigured back-end set-up to use Mongoose/MongoDB for storing users. Currently accepts new user sign-up and existing user log-in.

--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,65 @@
+var bcrypt = require('bcrypt');
+var mongoose = require('mongoose');
 var db = require('../config');
-var bcrypt = require('bcrypt-nodejs');
-var Promise = require('bluebird');
+var Q = require('q');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function(){
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
-    });
-  },
-  hashPassword: function(){
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
+// static salt
+var SALT_WORK_FACTOR = 10;
+
+// create the user schema for new users
+var UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, index: { unique: true } },
+  password: { type: String, required: true},
+  timestamp: {type: Date, default: Date.now},
+  salt: String
 });
 
+UserSchema.methods.comparePassword = function(candidatePassword) {
+  var defer = Q.defer();
+  var savedPassword = this.password;
+  bcrypt.compare(candidatePassword, savedPassword, function (err, isMatch) {
+    if (err) {
+      defer.reject(err);
+    } else {
+      defer.resolve(isMatch);
+    }
+  });
+  return defer.promise;
+};
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+
+  // only hash the password if it has been modified (or new)
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
+
+    // hash the password with salt
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) {
+        return next(err);
+      }
+
+      // overwrite the cleartext password with the hashed one
+      user.password = hash;
+      user.salt = salt;
+      next();
+    });
+  });
+});
+
+
+// define user model (mongoose models are equivalent of collections, so users file is irrelevant)
+var User = mongoose.model('User', UserSchema);
+
+// export for use in handler functions
 module.exports = User;
+
+
